339a548001023f309bb89139612cced7a7fe668e
yudhaabadi <85068199+yudhaabadi@users.noreply.github.com>
2021-07-19 22:10:32 +0700

    Add files via upload

---
+++ berita.php
@@ -0,0 +1,84 @@
+<!-- Blog -->
+	<section class="blog bgwhite p-t-94 p-b-65">
+		<div class="container">
+			<div class="sec-title p-b-52">
+				<h3 class="m-text5 t-center">
+					Review Pengguna
+				</h3>
+			</div>
+
+			<div class="row">
+				<div class="col-sm-10 col-md-4 p-b-30 m-l-r-auto">
+					<!-- Block3 -->
+					<div class="block3">
+						<a href="blog-detail.html" class="block3-img dis-block hov-img-zoom">
+							<img src="<?php echo base_url() ?>assets/template/images/fahr.jpg" alt="IMG-BLOG">
+						</a>
+
+						<div class="block3-txt p-t-14">
+							<h4 class="p-b-7">
+								<a href="blog-detail.html" class="m-text11">
+									Jas Hitam 
+								</a>
+							</h4>
+
+							<span class="s-text6">By</span> <span class="s-text7">Fakhri Azhari</span>
+							<span class="s-text6">on</span> <span class="s-text7">July 22, 2019</span>
+
+							<p class="s-text8 p-t-16">
+								Pokoknya puas deh sewa baju diyourstyle, lebih hemat dan nggak ketinggalan model.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-10 col-md-4 p-b-30 m-l-r-auto">
+					<!-- Block3 -->
+					<div class="block3">
+						<a href="blog-detail.html" class="block3-img dis-block hov-img-zoom">
+							<img src="<?php echo base_url() ?>assets/template/images/windi.jpg" alt="IMG-BLOG">
+						</a>
+
+						<div class="block3-txt p-t-14">
+							<h4 class="p-b-7">
+								<a href="blog-detail.html" class="m-text11">
+									Baju Kreasi Karnaval
+								</a>
+							</h4>
+
+							<span class="s-text6">By</span> <span class="s-text7">Windi Taruna Agustin</span>
+							<span class="s-text6">on</span> <span class="s-text7">July 18, 2019</span>
+
+							<p class="s-text8 p-t-16">
+								Jika ada acara tetentu nggak bingung lagi mau cari baju kemana, dengan yourstyle semuanya jadi mudah. makasih histyle
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-10 col-md-4 p-b-30 m-l-r-auto">
+					<!-- Block3 -->
+					<div class="block3">
+						<a href="blog-detail.html" class="block3-img dis-block hov-img-zoom">
+							<img src="<?php echo base_url() ?>assets/template/images/windi3.jpg" alt="IMG-BLOG">
+						</a>
+
+						<div class="block3-txt p-t-14">
+							<h4 class="p-b-7">
+								<a href="blog-detail.html" class="m-text11">
+									Jas Hitam Trendy
+								</a>
+							</h4>
+
+							<span class="s-text6">By</span> <span class="s-text7">Oktavio Rivaldo</span>
+							<span class="s-text6">on</span> <span class="s-text7">July 2, 2019</span>
+
+							<p class="s-text8 p-t-16">
+								Jasnya pas dan nggak ada lecet, nyaman deh pas di pake.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
